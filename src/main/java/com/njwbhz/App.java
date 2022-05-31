8f50e3337978df59d36b42d53568cb9578261cb6
wanglinhu <wanglinhu@qq.com>
2022-05-31 16:01:56 +0800

    hotfix的第二次添加

--- src/main/java/com/njwbhz/App.java
+++ src/main/java/com/njwbhz/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("hehe world");
         System.out.println("haha world");
+        System.out.println("您好");
     }
 }
